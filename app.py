41cc40094276fa2954664dec75ebaa3b77d1990e
ddleon8509 <65253116+ddleon8509@users.noreply.github.com>
2022-08-18 12:13:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
+
 import csv
+import re
+import meraki
+import json
 
-output = []
-inventory = []
+configs = json.load(open('config.json'))
+dashboard = meraki.DashboardAPI(configs['configs']['API_KEY'])
 
-def main():
+mgmtIf = {
+    'wan1': {
+        'wanEnabled': 'enabled', 
+        'usingStaticIp': False,
+        'staticIp': '',
+        'staticSubnetMask': '',
+        'staticGatewayIp': '',
+        'staticDns': [],
+        'vlan': configs['configs']["MgmtVlan"]
+    },
+    'wan2': {
+        'wanEnabled': 'disabled',
+        'usingStaticIp': False
+    }
+}
+
+def findSector(bldgName):
+    for s in configs['configs']["sectors"]:
+        key, values = list(s.items())[0]
+        if bldgName in values:        
+            return key
+    return 'sector-0'
+
+def process_files():
+    output = []
+    inventory = []
     with open('onboarding.csv', 'r') as merakiData:
         csvReader = csv.reader(merakiData, delimiter = ',')
         lineCount = 0
@@ -28,6 +57,46 @@ def main():
         csvWriter.writerow(['Box','NewSerialNumber','Name','IpAddr','OldSerialNumber'])
         for i in output:
             csvWriter.writerow(i)
+
+def main():
+	inventory = []
+
+	with open('meraki.csv', 'r') as merakiData:
+	csvReader = csv.reader(merakiData, delimiter = ',')
+	lineCount = 0
+	for row in csvReader:
+		if lineCount != 0:
+		tags = []
+		temp = re.search(r'^(\w+)-([A-Za-z]+)', row[2])
+		if temp is not None:
+			 tags.append(temp[1].lower())
+			 tags.append('bldg-' + temp[2].lower())
+			 if temp[1].lower() == configs['configs']["locations"]["0"]:
+			tags.append(findSector(temp[2].lower()))
+		inventory.append({'sn': row[1], 'box': row[0], 'name': row[2], 'tags': tags})
+		lineCount += 1
+	for i in inventory:
+
+	# Claiming APs per network
+
+	if configs['configs']["locations"]["0"] in i['tags']:
+		response = dashboard.networks.claimNetworkDevices(configs['configs']["networks"]["0"], [i['sn']])
+	elif configs['configs']["locations"]["1"] in i['tags']:
+		response = dashboard.networks.claimNetworkDevices(configs['configs']["networks"]["1"], [i['sn']])
+	elif configs['configs']["locations"]["2"] in i['tags']:
+		response = dashboard.networks.claimNetworkDevices(configs['configs']["networks"]["2"], [i['sn']])
+	elif configs['configs']["locations"]["3"] in i['tags']:
+		response = dashboard.networks.claimNetworkDevices(configs['configs']["networks"]["3"], [i['sn']])
+	else:
+		print(r'sn: {i["sn"]}. Failed to be added to a network')
+
+	# Naming and assigning tags to APs
+
+	response = dashboard.devices.updateDevice(i['sn'], name=i['name'], tags=i['tags'])
+
+	# Configuring Mgmt If
+
+	response = dashboard.devices.updateDeviceManagementInterface(i['sn'], wan1 = mgmtIf['wan1'], wan2 = mgmtIf['wan2']) 
 
 if __name__ == '__main__':
 	main()
